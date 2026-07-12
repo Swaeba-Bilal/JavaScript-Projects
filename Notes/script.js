8f53e2fe78c9bcee4c711652d6916f34266635d5
Swaeba-Bilal <swaibabilal02@gmail.com>
2026-07-12 11:41:29 +0500

    Added the Notes App

---
+++ Notes/script.js
@@ -0,0 +1,45 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes") || "";
+}
+
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click", () => {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+
+    img.src = "images/delete.png";
+
+    inputBox.appendChild(img);
+    notesContainer.appendChild(inputBox);
+
+    updateStorage();
+});
+
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+});
+
+notesContainer.addEventListener("input", function () {
+    updateStorage();
+});
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+});
